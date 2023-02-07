e48e32ad21c59006a192470a90afb781ea183fff
Danny Pham <dpham01@cs.washington.edu>
2023-02-06 21:06:13 -0800

    Basic database methods + database tests

--- db.py
+++ db.py
@@ -5,3 +5,12 @@ CONNECTION_STRING = "mongodb+srv://polygraph:polygraph#123@cluster0.w5ydh5i.mong
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('Cluster0')
 user_collection = pymongo.collection.Collection(db, 'user_collection')
+
+def get_user_history(user_id):
+    return user_collection.find_one({"_id":user_id})["history"]
+
+def add_user(user_id):
+    user_collection.insert_one({"_id:":user_id}, {"history":[]})
+
+def add_user_history(user_id, history_entry):
+    user_collection.update_one({"_id":user_id}, {"$addToSet":{"history":history_entry}})
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,16 +1,26 @@
-from app import app
 from flask import Flask
 from flask_pymongo import pymongo
 import pytest
 
-class TestDB:
-    def __init__(self, cluster):
-        db = cluster["test"]
-        collection = db["test"]
+CONNECTION_STRING = "mongodb+srv://polygraph:polygraph#123@cluster0.w5ydh5i.mongodb.net/?retryWrites=true&w=majority"
+cluster = pymongo.MongoClient(CONNECTION_STRING)
+db = cluster["test"]
+test_collection = pymongo.collection.Collection(db, "test_collection")
 
-    def test_insert_one(self):
-        post = {"_id": 0, "history": []}
-        self.collection.insert_one(post)
+# def add_test_data():
+#     topics = ["vaccines", "legislation", "conspiracy", "foreign affairs", "pseudoscience", "COVID-19", "election fraud"]
+#     for i in range(100):
+#         post = {"_id": "user" + str(i), "history": [topics[i % len(topics)]]}
+#         #test_collection.insert_one(post)
 
-        results = self.collection.find({"_id":0})
-        assert not results
+# add_test_data()
+
+# def test_retrieve_history():
+#     # in the future this will test retrieving actual history calling a method in db.py
+#     results = test_collection.find_one({"_id":"user1"})
+#     assert results[0]["history"] == "legislation"
+#     results = test_collection.find({"history":"vaccines"})
+#     i = 1
+#     for result in results:
+#         assert result["_id"] == "user" + str(i * 7)
+#         i += 1
