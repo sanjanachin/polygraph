d6d79c78982601fb182dcfc69b219fd7e5e44e51
clynch20 <clynch20@uw.edu>
2023-02-09 12:41:09 -0800

    updated model tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,19 +6,19 @@ class TestModel:
     client = app.test_client()
 
     def test_model_simple_false(self):
-        test_str = "Obama was the first U.S. president"
+        test_str = "Obama was the first U.S. president."
         response = app.test_client().post('/model', json={"text": test_str})
-        print(response)
-        assert response == "false"
+        assert response.status_code == 200
+        assert(response.text.split()[0].strip(".") == "False")
 
     def test_model_simple_true(self):
-        test_str = "Earth has one moon"
+        test_str = "Earth has one moon."
         response = app.test_client().post('/model', json={"text": test_str})
-        print(response)
-        assert response == "true"
+        assert response.status_code == 200
+        assert(response.text.split()[0].strip(".") == "True")
 
     def test_model_complex(self):
-        test_str = "Donald Trump won the 2020 election"
+        test_str = "Donald Trump won the 2020 election."
         response = app.test_client().post('/model', json={"text": test_str})
-        print(response)
-        assert response == "false"
+        assert response.status_code == 200
+        assert(response.text.split()[0].strip(".") == "False")
