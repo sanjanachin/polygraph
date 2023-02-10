ee3775be608ecb26a0035fa38cabca799e69fd79
Danny Pham <dpham01@cs.washington.edu>
2023-02-09 18:14:44 -0800

    finished DB implementation, tests WIP

--- db.py
+++ db.py
@@ -1,16 +1,32 @@
 from flask import Flask
 from flask_pymongo import pymongo
 
-CONNECTION_STRING = "mongodb+srv://polygraph:polygraph#123@cluster0.w5ydh5i.mongodb.net/?retryWrites=true&w=majority"
+CONNECTION_STRING = "mongodb+srv://polygraph:PASSWORD@cluster0.w5ydh5i.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('Cluster0')
 user_collection = pymongo.collection.Collection(db, 'user_collection')
 
+# Adds a new user to the database
+# user_id: the id to be associated with this user for all future DB use
+# Returns True upon successful addition, and False if the user_id passed
+# is already in use
+def add_user(user_id):
+    if (not user_collection.find_one({"_id":user_id})):
+        return False
+    user_collection.insert_one({"_id:":user_id}, {"history":[]})
+    return True
+
+# Retrieves a user's history data
+# user_id: the associated id of the user that was assigned when the user was added
+# returns: an array of pairs, with the first element of each pair being the user's
+# query and the second element being the result from that query. Will return an empty
+# array if the user has no history or if the user does not exist
 def get_user_history(user_id):
     return user_collection.find_one({"_id":user_id})["history"]
 
-def add_user(user_id):
-    user_collection.insert_one({"_id:":user_id}, {"history":[]})
-
-def add_user_history(user_id, history_entry):
-    user_collection.update_one({"_id":user_id}, {"$addToSet":{"history":history_entry}})
+# Inserts a new entry into a user's history data
+# user_id: the associated id of the user that was assigned when the user was added
+# query: the user's input
+# result: what the program returned from the user's input
+def add_user_history(user_id, query, result):
+    user_collection.update_one({"_id":user_id}, {"$addToSet":{"history":[query, result]}})
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,24 +1,24 @@
-from flask import Flask
-from flask_pymongo import pymongo
-import pytest
+# from flask import Flask
+# from flask_pymongo import pymongo
+# import pytest
 
-CONNECTION_STRING = "mongodb+srv://polygraph:a1JbnPxslP8gN3Sm@cluster0.w5ydh5i.mongodb.net/?retryWrites=true&w=majority"
-cluster = pymongo.MongoClient(CONNECTION_STRING)
-db = cluster["Cluster0"]
-data = pymongo.collection.Collection(db, "test_users")
+# CONNECTION_STRING = "mongodb+srv://polygraph:PASSWORD@cluster0.w5ydh5i.mongodb.net/?retryWrites=true&w=majority"
+# cluster = pymongo.MongoClient(CONNECTION_STRING)
+# db = cluster["Cluster0"]
+# data = pymongo.collection.Collection(db, "test_users")
 
-def add_test_data():
-    topics = ["vaccines", "legislation", "conspiracy", "foreign affairs", "pseudoscience", "COVID-19", "election fraud"]
-    for i in range(100):
-        post = {"_id": "user" + str(i), "history": [topics[0:(i % len(topics))]]}
-        data.update_one({"_id": "user" + str(i)}, {"$set":post}, upsert=True)
+# def add_test_data():
+#     topics = ["vaccines", "legislation", "conspiracy", "foreign affairs", "pseudoscience", "COVID-19", "election fraud"]
+#     for i in range(100):
+#         post = {"_id": "user" + str(i), "history": [topics[0:(i % len(topics))]]}
+#         data.update_one({"_id": "user" + str(i)}, {"$set":post}, upsert=True)
 
-add_test_data()
+# add_test_data()
 
-def test_retrieve_history_from_testdb():
-    result = data.find_one({"_id":"user46"})
-    assert result["history"][0][0] == "vaccines"
-    assert result["history"][0][1] == "legislation"
-    assert result["history"][0][2] == "conspiracy"
-    assert result["history"][0][3] == "foreign affairs"
+# def test_retrieve_history_from_testdb():
+#     result = data.find_one({"_id":"user46"})
+#     assert result["history"][0][0] == "vaccines"
+#     assert result["history"][0][1] == "legislation"
+#     assert result["history"][0][2] == "conspiracy"
+#     assert result["history"][0][3] == "foreign affairs"
     
