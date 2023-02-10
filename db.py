eedab5d33e3573905c6a3992d12a2fa8e7023f37
Danny Pham <dpham01@cs.washington.edu>
2023-02-09 20:01:42 -0800

    set up DB with secret variables for URI keys

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask
 from flask_pymongo import pymongo
 
-CONNECTION_STRING = "mongodb+srv://polygraph:PASSWORD@cluster0.w5ydh5i.mongodb.net/?retryWrites=true&w=majority"
+CONNECTION_STRING = "mongodb+srv://" + os.environ.get("MONGODB_USERNAME") + ":" + os.environ.get("MONGODB_PASSWORD") + "@cluster0.w5ydh5i.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('Cluster0')
 user_collection = pymongo.collection.Collection(db, 'user_collection')
