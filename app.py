3d9f5b4766e398dd62f7250bbe0c82663dbb7e04
clynch20 <clynch20@uw.edu>
2023-02-13 18:38:27 -0800

    app.py model expansion

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route('/')
 def flask_mongodb_atlas():
     return Response("Polygraph Backend & Flask Mongodb Atlas!", status=200)
-if __name__ == '__main__':
-    app.run(port=8000)
 
 #test to insert data to the data base
 @app.route("/test")
@@ -34,19 +32,35 @@ def parse_request():
     print(req_data)
     return Response(text, 200)
 
+prompt_string = "The following text contains misinformation: True or false?: "
+
 # model response using openai direct completion
 @app.route("/model", methods=["POST"])
 def model_basic_req_resp():
+
+    def text_to_bool(text):
+        get_first = text.split()[0].strip(".")
+        if get_first == "True":
+            return Response(True, 200)
+        elif get_first == "False":
+            return Response(False, 200)
+        else:
+            return Response(False, 400)
+
     text = request.get_json()["text"]
     # a basic response pattern for davinci 003
     response = openai.Completion.create(
         model="text-davinci-003",
         # note specific prompt will be subject to change depending on test accuracy
         # will eventually integrate prompt data from frontend endpoint
-        prompt="True or false?: " + text,
+        prompt=prompt_string + text,
         temperature=0.6,
         # small token size for now, will expand with greater testing
         max_tokens=100,
     )
     # for now take the first choice with no fine-tuning
-    return Response(response.choices[0].text, 200)
+    output = response.choices[0].text
+    return text_to_bool(output).get_data()
+
+if __name__ == '__main__':
+    app.run(port=8000, ssl_context='adhoc')
