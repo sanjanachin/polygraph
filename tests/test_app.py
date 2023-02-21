be9680813701c4309955d9db71e220b0b22de691
sanjanachintalapati <sanjanac@cs.washington.edu>
2023-02-21 02:20:04 -0800

    More api handling testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 from app import app
 import pytest
+import json
 
 class TestApp:
     app.testing = True
@@ -14,6 +15,14 @@ class TestApp:
     #     response = app.test_client().post('/misinformation', json={"text": test_str})
     #     assert response.status_code == 200
     #     assert response.text == test_str
+
+    def test_misinformation_handling(self):
+        test_str = "This is a test"
+        headers = {'header': 'header-info'}
+        response = app.test_client().options('/misinformation', json={"text": test_str}, headers=headers)
+        assert response.status_code == 204
+        
+
 
     def test_misinformation_error_handling(self):
         with pytest.raises(Exception, match="Request text is empty"):
