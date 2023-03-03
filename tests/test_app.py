0274556c0da401f0e4b4654c8760289f4f515efa
sanjanachin <64334304+sanjanachin@users.noreply.github.com>
2023-03-02 19:53:10 -0800

    Fix test_app to reflect new expected request body in /misinformation

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,11 +13,11 @@ class TestApp:
     def test_misinformation_handling(self):
         test_str = "This is a test"
         headers = {'header': 'header-info'}
-        response = app.test_client().options('/misinformation', json={"text": test_str}, headers=headers)
+        response = app.test_client().options('/misinformation', json={"text": test_str, "user": "user"}, headers=headers)
         assert response.status_code == 204
 
 
     def test_misinformation_error_handling(self):
         with pytest.raises(Exception, match="Request text is empty"):
             test_str = ""
-            response = app.test_client().post('/misinformation', json={"text": test_str})
+            response = app.test_client().post('/misinformation', json={"text": test_str, "user": "user"})
